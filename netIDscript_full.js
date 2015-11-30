8ce0c089145ef6dc0ab3fe9cfe5a3f7017aae02c
Sean Keenan <skeenan@nd.edu>
2015-11-30 17:55:01 -0500

    Update netIDscript_full.js

--- netIDscript_full.js
+++ netIDscript_full.js
@@ -1,13 +1,4 @@
-javascript:(function(){
-	if (window.location.indexOf("https://apps.nd.edu/webdirectory/directory.cfm") > -1) {
-		alert('Connection successful');
-		//jQuery("input[name=cn]").val("Bilgicer");
-		//jQuery("form[name=form7]").submit();
-		//alert("Success");
-	}
-})();
-
-(function(win, doc, $, undefined) {
+javascript:(function(win, doc, $, undefined){
     'use strict';
     
     // Don't run if jQuery isn't loaded
@@ -18,9 +9,8 @@ javascript:(function(){
     // Code goes here.
     var fillForm = function() {
     	$('input[name=cn]').val(data.name);
-  };
-  
-  fillForm();
-  $('#form').submit();
-
-  }(window, window.document, window.jQuery));
+    };
+    
+    fillForm();
+    $('#form').submit();
+})(window, window.document, window.jQuery);
