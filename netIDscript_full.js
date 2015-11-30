0ee2bbfae00d2d93de7244d39f587f3db626d12f
Sean Keenan <skeenan@nd.edu>
2015-11-30 11:59:15 -0500

    Update netIDscript_full.js

--- netIDscript_full.js
+++ netIDscript_full.js
@@ -6,3 +6,90 @@ javascript:(function(){
 		//alert("Success");
 	}
 })();
+
+(function(win, doc, $, undefined) {
+    'use strict';
+    
+    // Don't run if jQuery isn't loaded
+    if (typeof window.jQuery === 'undefined') {
+      return;
+    }
+    
+    // Code goes here.
+    var _rand = function(min, max) {
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    $.getScript('//cdnjs.cloudflare.com/ajax/libs/Faker/0.7.2/MinFaker.js').done(function() {
+    	// run our functions from here
+    }).fail(function() {
+    	win.console.error('ERROR: FakerJS not loaded!');
+    });
+    
+    var FormData = function(faker) {
+    	this.faker     = faker;
+    
+    	this.randomWord = faker.Internet.domainWord();
+
+    	this.username  = 'fake_' + this.randomWord;
+    	this.username  += _rand(100,9999);
+
+    	// set this value to your password specifications
+    	this.password  = 'pass1234';
+
+    	this.name      = faker.Name.findName();
+
+    	this.address1  = faker.Address.streetAddress();
+    	this.city      = faker.Address.city();
+    	this.state     = faker.random.br_state_abbr();
+    	this.zip       = faker.Address.zipCode();
+    	
+    	// Chris' actual credit card number
+    	this.cc        = '4242 4242 4242 4242';
+    	this.exp1      = _rand(1,12);
+    	this.exp2      = _rand(14,22);
+    	this.cvv       = _rand(100,999);
+    };
+    
+    var fillForm = function() {
+    	data = new FormData(window.Faker);
+
+    	$('#name').val(data.name);
+    	$('#username').val(data.username);
+    	$('#cc').val(data.cc);
+    	$('#exp-1').val(data.exp1);
+    	$('#exp-2').val(data.exp2);
+    	$('#cvv').val(data.cvv);
+    	$('#address').val(data.address1);
+    	$('#city').val(data.city);
+    	$('#state').val(data.state);
+    	$('#zip').val(data.zip);
+    	$('#pw').val(data.password);
+    	$('#pw-repeat').val(data.password);
+
+    	data.randomizeRadio($('[name="radio-choice"]'));
+
+    	// Randomize all select boxes
+    	$('select').each(function() {
+      	data.randomizeSelect(this);
+    	});
+
+    	// Randomize all checkboxes
+    	$('input[type="checkbox"').each(function() {
+      	data.randomizeCheckbox(this);
+	});
+
+    	// Randomize all textareas
+    	$('textarea').each(function() {
+      	data.randomizeParagraph(this);
+    	});
+    
+    	// Randomize all emails
+    	$('input[type="email"').each(function() {
+      	data.randomizeEmail(this);
+    	});
+  };
+  
+  $('#form').submit();
+
+  }(window, window.document, window.jQuery));
