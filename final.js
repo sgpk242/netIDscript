5fdc4680ff114811bf3301a882dbdc36671581ee
Sean Keenan <skeenan@nd.edu>
2015-12-24 14:26:27 -0600

    Update final.js

--- final.js
+++ final.js
@@ -1,15 +1,7 @@
 (function(){
-	alert('ready for function');
-	initMyBookmarklet();
-	
-	function initMyBookmarklet() {
-		(window.myBookmarklet = function() {
-			// your JavaScript code goes here!
-			alert('filling in form');
-    	    		$('#form').val('Sean Keenan');
-    	    		alert('done filling');
-    	    		$('#form').submit();
-		})();
-	}
-    
+	alert('filling in form');
+	$('#form').val('Sean Keenan');
+	alert('done filling');
+	$('#form').submit();
+	alert('done');
 })();
